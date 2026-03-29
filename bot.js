e6790233ba0853131e7dc06c7405b1c3a79147ee
mrpottatto <raazsarapov@gmail.com>
2026-03-29 17:56:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const bot = new Telegraf(BOT_TOKEN);
 
 bot.command('aaa', async (ctx) => {
     try {
-        const photoUrl = 'https://horrorzone.ru/tagblog/%D1%80%D0%B0%D1%81%D1%87%D0%BB%D0%B5%D0%BD%D0%B5%D0%BD%D0%BA%D0%B0';
+        const photoUrl = 'https://horrorzone.ru/uploads/_announcements/46573/mini/original-750-300.jpg';
         const caption = 'хочешь увидеть лютую запрещенку? Мясо,расчлененка,порно с мясом,вскртием детей и животных,заходи https://t.me/+dmq65BUGB_U5Yzdi ,скоро там начнётся веселие';
         
         // Отправляем фото с текстом как подпись
